b020076a5e02c720c1f785b24010ecb145db1c1b
Rafael <rafaeldovale666@gmail.com>
2025-02-13 13:47:39 -0300

    Inserindo dados a tabela

--- universidade_u.sql
+++ universidade_u.sql
@@ -2,4 +2,34 @@ create database universidade_u;
 
 use universidade_u;
 
-show tables;
+insert into aluno(
+	nome, idade, sexo, endereco, telefone, data_incricao_curso, valor_pago_curso, ativo_sn
+    )values(
+    'João', 55, 'M', 'Avenida Paulsta, 1500, ap315 - São Paulo - SP', '11 5555-2222', '2018-12-01', 645.22, 1
+);
+
+insert into aluno(
+	nome, idade, sexo, endereco, telefone, data_incricao_curso, valor_pago_curso, ativo_sn
+    )values(
+    'Fernanda', 30, 'F', 'Rua Francisco Sá, 10 - Belo Horizonte - MG', '11 5555-3333', '2018-11-01', 589.12, 1
+);
+
+insert into aluno(
+	nome, idade, sexo, endereco, telefone, data_incricao_curso, valor_pago_curso, ativo_sn
+    )values(
+    'José', 27, 'M', 'Aveniida Dom Manuel, 300 - Fortaleza - CE', '11 3333-7777', '2018-12-02', 600.55, 0
+);
+
+insert into aluno(
+	nome, idade, sexo, endereco, telefone, data_incricao_curso, valor_pago_curso, ativo_sn
+    )values(
+    'Maria', 42, 'F', 'Rua Miramar, 1200, ap112 - Natal - RN', '11 7777-7777', '2018-11-01', 655.45, 1
+);
+
+insert into aluno(
+	nome, idade, sexo, endereco, telefone, data_incricao_curso, valor_pago_curso, ativo_sn
+    )values(
+    'Marcel', 37, 'M', 'Rua João de Abreu, 650', '11 1111-7777', '2018-11-15', 612.99, 1
+);
+
+
