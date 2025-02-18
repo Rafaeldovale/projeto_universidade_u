bdc43c003fc07d3c53272664837658ff69566045
Rafael <rafaeldovale666@gmail.com>
2025-02-18 14:41:38 -0300

    Criando tabela telefone e add sua cardinalidade

--- universidade_u.sql
+++ universidade_u.sql
@@ -1,72 +1,17 @@
 use universidade_u;
 
 select * from aluno;
+select * from telefone;
 
-alter table aluno add logadouro varchar(100);
-alter table aluno add numero varchar(10);
-alter table aluno add complemento varchar(20);
-alter table aluno add bairro varchar(100);
-alter table aluno add cidade varchar(50);
-alter table aluno add estado char(2);
+desc aluno;
 
-update 
-	aluno
-set
-	logadouro = 'Avenida Paulsta',
-    numero = '1500',
-    complemento = 'ap315',
-    bairro = 'Bela Vista',
-    cidade = 'São Paulo ',
-    estado = 'SP'
-where
-	idaluno = 1;
+create table telefone(
+	idtelefone int auto_increment primary key,
+    numero varchar(20),
+    tipo char(3)
+    );
     
-update 
-	aluno
-set
-	logadouro = 'Rua Francisco Sá',
-    numero = '10',
-    complemento = '',
-    bairro = 'Gutierrez',
-    cidade = 'Belo Horizonte',
-    estado = 'MG'
-where
-	idaluno = 2;
-    
-update 
-	aluno
-set
-	logadouro = 'Rua Miramar',
-    numero = '1200',
-    complemento = 'ap112',
-    bairro = 'Rocas',
-    cidade = 'Natal',
-    estado = 'RN'
-where
-	idaluno = 3;
-    
-update 
-	aluno
-set
-	logadouro = 'Rua João Abreu',
-    numero = '650',
-    complemento = '',
-    bairro = 'Setor Oeste',
-    cidade = 'Goiânia',
-    estado = 'GO'
-where
-	idaluno = 4;
-    
-update 
-	aluno
-set
-	logadouro = 'Avenida Dom Manuel',
-    numero = '300',
-    complemento = '',
-    bairro = 'Centro',
-    cidade = 'Fortaleza',
-    estado = 'CE'
-where
-	idaluno = 5;
+insert into telefone(numero, tipo) values('11 5555-2222', 'res');
+insert into telefone(numero, tipo) values('11 3333-4444', 'com');
 
-alter table aluno drop endereco;
+alter table aluno drop column telefone;
