e24cd7f7bec45cfab83fd2c3c1abfcd2bf785709
Rafael <rafaeldovale666@gmail.com>
2025-02-18 13:57:07 -0300

    dividindo campo endereço

--- universidade_u.sql
+++ universidade_u.sql
@@ -2,10 +2,71 @@ use universidade_u;
 
 select * from aluno;
 
-update aluno set cpf = '111.111.111-11' where nome = 'João';
-update aluno set cpf = '222.111.111-11' where nome = 'Fernanda';
-update aluno set cpf = '333.111.111-11' where nome = 'José';
-update aluno set cpf = '444.111.111-11' where nome = 'Maria';
-update aluno set cpf = '555.111.111-11' where nome = 'Marcel';
+alter table aluno add logadouro varchar(100);
+alter table aluno add numero varchar(10);
+alter table aluno add complemento varchar(20);
+alter table aluno add bairro varchar(100);
+alter table aluno add cidade varchar(50);
+alter table aluno add estado char(2);
 
-alter table aluno add idaluno int primary key auto_increment;
+update 
+	aluno
+set
+	logadouro = 'Avenida Paulsta',
+    numero = '1500',
+    complemento = 'ap315',
+    bairro = 'Bela Vista',
+    cidade = 'São Paulo ',
+    estado = 'SP'
+where
+	idaluno = 1;
+    
+update 
+	aluno
+set
+	logadouro = 'Rua Francisco Sá',
+    numero = '10',
+    complemento = '',
+    bairro = 'Gutierrez',
+    cidade = 'Belo Horizonte',
+    estado = 'MG'
+where
+	idaluno = 2;
+    
+update 
+	aluno
+set
+	logadouro = 'Rua Miramar',
+    numero = '1200',
+    complemento = 'ap112',
+    bairro = 'Rocas',
+    cidade = 'Natal',
+    estado = 'RN'
+where
+	idaluno = 3;
+    
+update 
+	aluno
+set
+	logadouro = 'Rua João Abreu',
+    numero = '650',
+    complemento = '',
+    bairro = 'Setor Oeste',
+    cidade = 'Goiânia',
+    estado = 'GO'
+where
+	idaluno = 4;
+    
+update 
+	aluno
+set
+	logadouro = 'Avenida Dom Manuel',
+    numero = '300',
+    complemento = '',
+    bairro = 'Centro',
+    cidade = 'Fortaleza',
+    estado = 'CE'
+where
+	idaluno = 5;
+
+alter table aluno drop endereco;
