6c9faaf09a960ac95a6367430845db38096f6db6
Rafael <rafaeldovale666@gmail.com>
2025-02-20 14:09:00 -0300

    populando a tabela endereco com os dados da tabela aluno , utilizando insert into

--- universidade_u.sql
+++ universidade_u.sql
@@ -1,16 +1,14 @@
 use universidade_u;
 
-CREATE TABLE ENDERECO (
-    idendereco int PRIMARY KEY,
-    logradouro varchar(100),
-    numero varchar(10),
-    complemento varchar(20),
-    bairro varchar(100),
-    cidade varchar(50),
-    estado char(2),
-    fk_idaluno int
-);
- 
- ALTER TABLE ENDERECO ADD CONSTRAINT fk_aluno_endereco
-    FOREIGN KEY (fk_idaluno)
-    REFERENCES ALUNO (idaluno);
+select * from aluno; 
+select * from endereco;
+
+alter table aluno rename column logadouro to logradouro;
+
+insert into endereco (
+	idendereco, logradouro, numero, complemento, bairro, cidade, estado, fk_idaluno
+    )
+select 
+	idaluno, logradouro, numero, complemento, bairro, cidade, estado, idaluno
+from 
+	aluno;
