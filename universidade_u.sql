89d9b42897fb646d79e61ffae09eab173bfd0bb4
Rafael <rafaeldovale666@gmail.com>
2025-02-24 14:20:36 -0300

    Alterando tabela endereco, inserindo nas colunas dados not null

--- universidade_u.sql
+++ universidade_u.sql
@@ -1,12 +1,23 @@
 use universidade_u;
 
-select * from telefone;
-desc telefone; 
+select * from endereco;
+desc endereco;
 
-alter table telefone modify column numero varchar(20) not null;
-alter table telefone modify column tipo enum('res', 'com', 'cel') not null;
-alter table telefone modify column fk_idaluno int not null;
-
-
+CREATE TABLE ENDERECO (
+    idendereco int PRIMARY KEY,
+    logradouro varchar(100) not null,
+    numero varchar(10) not null,
+    complemento varchar(20),
+    bairro varchar(100) not null,
+    cidade varchar(50) not null,
+    estado char(2) not null,
+    fk_idaluno int not null
+);
+alter table endereco  modify column logradouro varchar(100) not null;
+alter table endereco modify column numero varchar(10) not null;
+alter table endereco modify column bairro varchar(100) not null;
+alter table endereco modify column cidade varchar(50) not null;
+alter table endereco modify column estado char(2) not null;
+alter table endereco modify column fk_idaluno int not null;
 
 
