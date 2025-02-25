3d0a4f8d38ae44d98a74a79b13c34b6a94507bda
Rafael <rafaeldovale666@gmail.com>
2025-02-25 13:57:52 -0300

    Criando a constraint unique na tabela endereco no campo fk_idaluno

--- universidade_u.sql
+++ universidade_u.sql
@@ -1,14 +1,7 @@
 use universidade_u;
 
-select * from aluno;
+select * from endereco;
+desc endereco;
 
-alter table aluno add constraint uc_aluno_cpf unique(cpf);
-
-
-insert into aluno (nome, idade, email, cpf, sexo, data_incricao_curso, valor_pago_curso, ativo_sn)
-values('Rosa', 25, 'rosa@teste.com.br', '444.111.111-12', 'F', '2019-01-05', 625, 1);
-
-alter table aluno add constraint uc_aluno_email unique(email);
-
-desc aluno;
+alter table endereco add constraint uc_endereco_fkidaluno unique(fk_idaluno);
 
