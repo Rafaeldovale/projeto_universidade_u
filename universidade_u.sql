eb18c3e3558dc60fcd0da4ce58fcc909725e7f49
Rafael <rafaeldovale666@gmail.com>
2025-02-27 13:52:05 -0300

    criando a tabela gasto no modelo conceitual, logico e fisico

--- universidade_u.sql
+++ universidade_u.sql
@@ -1,7 +1,20 @@
 use universidade_u;
 
-select * from endereco;
-desc endereco;
-
-alter table endereco add constraint uc_endereco_fkidaluno unique(fk_idaluno);
-
+create table gasto(
+	idgasto int primary key auto_increment,
+    ano int not null,
+    tipo enum('previsto','realizado') not null,
+    jan float(10,2),
+    fev float(10,2),
+    mar float(10,2),
+    abr float(10,2),
+    mai float(10,2),
+    jun float(10,2),
+    jul float(10,2),
+    ago float(10,2),
+    `set` float(10,2),
+    `out` float(10,2),
+    nov float(10,2),
+    dez float(10,2)
+);
+select * from gasto;
